41f56d3a35a8b9eb3279baa21dfc9d7f8f6aac83
suriya-it19 <suriya.it19@bitsathy.ac.in>
2022-07-29 15:39:33 +0530

    added coroutines

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,9 +1,11 @@
 // HTTP requests
 // need to add dependencies in here to use kdelete
 
+import javafx.application.Application.launch
 import java.io.InputStream
 import javax.xml.ws.Response
 import khttp
+import java.io.File
 
 
 fun main(args: Array<String>) {
@@ -51,5 +53,71 @@ fun main(args: Array<String>) {
         process(response)
     } else {
         handleError(response)
+    }
+}
+
+class test {
+    fun add() {
+        try {
+            var add: Int? = null
+            add?.plus(1)
+        }
+        catch (e: NullPointerException) {
+
+        }
+    }
+}
+// Basic thing is threads - https://youtu.be/lmRzRKIsn1g
+// coroutine - lightweight thread to process asynchronous
+// it can be paused and resumed dynamically
+
+// launch is used to start and stop a coroutine
+// The async keyword is used to initiate a coroutine that computes a result.
+class Coroutine {
+    // runs in the main thread
+    fun ThreadExample() {
+        val job: Job = launch {
+            delay(1000)
+        }
+        job.join()
+    }
+
+    GlobalScope.launch {
+        delay(2000)
+    }
+}
+
+
+class SlidingWindow {
+    fun slide() {
+        val array = (1..10).toList()
+        val window = array.windowed(3, 1, true) {
+            it.joinToString(", ")
+        }
+        println(window)
+    }
+}
+
+// learn all the concepts with clear understanding
+// know why, when and how to use  those concepts
+data class Sample(var first: String, var second: String) {
+    fun add() {
+        val self: String? = null
+        self?.length
+        self!!.length
+    }
+}
+
+sealed class yellow {
+    val name = "hello"
+    class first {
+        fun print() {
+            println(name) // cannot read
+        }
+    }
+    inner class second {
+        fun print() {
+            println(name) // can read
+        }
     }
 }
